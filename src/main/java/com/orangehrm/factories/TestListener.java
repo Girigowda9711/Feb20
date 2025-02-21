52de9bd490b157fb4a768e8a97986b4494963c5d
Girigowda9711 <girigowda9711@gmail.com>
2025-02-21 20:31:29 +0530

    tehn

--- src/main/java/com/orangehrm/factories/TestListener.java
+++ src/main/java/com/orangehrm/factories/TestListener.java
@@ -15,58 +15,59 @@ import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.MediaEntityBuilder;
 
-public class TestListener implements ITestListener{
-	
-	 private static ExtentReports extent;
+public class TestListener implements ITestListener {
 
-	    @Override
-	    public void onStart(ITestContext context) {
-	        extent = ExtentManager.getInstance();
-	    }
+	private static ExtentReports extent;
+	private WebDriver driver;
 
-	    @Override
-	    public void onTestStart(ITestResult result) {
-	        ExtentTest test = extent.createTest(result.getMethod().getMethodName());
-	        ExtentManager.setTest(test);
-	    }
+	@Override
+	public void onStart(ITestContext context) {
+		extent = ExtentManager.getInstance();
+	}
 
-	    @Override
-	    public void onTestSuccess(ITestResult result) {
-	        ExtentManager.getTest().pass("Test Passed");
-	    }
+	@Override
+	public void onTestStart(ITestResult result) {
+		ExtentTest test = extent.createTest(result.getMethod().getMethodName());
+		ExtentManager.setTest(test);
+	}
 
-	    @Override
-	    public void onTestFailure(ITestResult result) {
-	        WebDriver driver = DriverFactory.getDriver();
-	        String screenshotPath = captureScreenshot(driver, result.getMethod().getMethodName());
-	        
-	        try {
-	            ExtentManager.getTest().fail("Test Failed: " + result.getThrowable(),
-	                MediaEntityBuilder.createScreenCaptureFromPath(screenshotPath).build());
-	        } catch (Exception e) {
-	            e.printStackTrace();
-	        }
-	    }
+	@Override
+	public void onTestSuccess(ITestResult result) {
+		ExtentManager.getTest().pass("Test Passed");
+	}
 
-	    @Override
-	    public void onTestSkipped(ITestResult result) {
-	        ExtentManager.getTest().skip("Test Skipped");
-	    }
+	@Override
+	public void onTestFailure(ITestResult result) {
+		driver = DriverFactory.getDriver();
+		String screenshotPath = captureScreenshot(driver, result.getMethod().getMethodName());
 
-	    @Override
-	    public void onFinish(ITestContext context) {
-	        extent.flush();
-	    }
+		try {
+			ExtentManager.getTest().fail("Test Failed: " + result.getThrowable(),
+					MediaEntityBuilder.createScreenCaptureFromPath("."+screenshotPath).build());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
-	    private String captureScreenshot(WebDriver driver, String testName) {
-	        String screenshotPath = "screenshots/" + testName + ".png";
-	        File src = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-	        try {
-	            FileUtils.copyFile(src, new File(screenshotPath));
-	        } catch (IOException e) {
-	            e.printStackTrace();
-	        }
-	        return screenshotPath;
-	    }
+	@Override
+	public void onTestSkipped(ITestResult result) {
+		ExtentManager.getTest().skip("Test Skipped");
+	}
+
+	@Override
+	public void onFinish(ITestContext context) {
+		extent.flush();
+	}
+
+	private String captureScreenshot(WebDriver driver, String testName) {
+		String screenshotPath = "./screenshots/" + testName + ".png";
+		File src = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+		try {
+			FileUtils.copyFile(src, new File(screenshotPath));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return screenshotPath;
+	}
 
 }
